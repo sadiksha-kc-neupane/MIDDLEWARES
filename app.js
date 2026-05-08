e1b39064c9d9fe73d49db09f90d0d7d8b53175d3
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-05-08 18:00:20 +0545

    1st middleware

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use((req, res) => {
+  let { query } = req.query;
+  console.log("query", query);
+  res.send("this is middleware");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
